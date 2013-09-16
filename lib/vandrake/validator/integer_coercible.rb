56096e1833ac4747c2da0858e5fa1541340f43fb
Adam Borocz <boroczadam@gmail.com>
2013-09-16 19:01:49 +0100

    Add new IntegerCoercible validator

---
+++ lib/vandrake/validator/integer_coercible.rb
@@ -0,0 +1,32 @@
+module Vandrake
+  module Validator
+    # Used to validate whether a given value can be reliable coerced into an Integer
+    class IntegerCoercible < Base
+
+      @error_codes = {
+        :not_integer => "must be an integer"
+      }
+
+      @is_raw = true
+
+      protected
+        # Run validation. Returns True if the given value can be safely converted
+        # to an Integer type.
+        #
+        # @param value
+        # @return [TrueClass, FalseClass] Validation success
+        def self.run_validator(value, params={})
+          return true if value.nil?
+
+          begin
+            Integer(value)
+            return true
+          rescue ArgumentError
+            set_error :not_integer
+            return false
+          end
+        end
+      # end Protected
+    end
+  end
+end
---
+++ spec/validator/integer_coercible_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Vandrake::Validator::IntegerCoercible do
+  context "::validate" do
+    subject(:validator) { described_class }
+
+    context "when called with nil" do
+      it { validator.validate(nil).should be_true }
+      its(:last_error_code) { should be_nil }
+      its(:last_error) { should be_nil }
+    end
+
+    context 'when called with 100' do
+      it { validator.validate(100).should be_true }
+      its(:last_error_code) { should be_nil }
+      its(:last_error) { should be_nil }
+    end
+
+    context 'when called with "100"' do
+      it { validator.validate("100").should be_true }
+      its(:last_error_code) { should be_nil }
+      its(:last_error) { should be_nil }
+    end
+
+    context 'when called with "one"' do
+      it { validator.validate("one").should be_false }
+      its(:last_error_code) { should eq(:not_integer) }
+      its(:last_error) { should eq("must be an integer") }
+    end
+  end
+end
